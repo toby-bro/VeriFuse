783a568bb30ea293858c1cfca3f44f292e7021bc
toby <toby.bro@pm.me>
2025-05-12 16:48:11 -0700

    chore: color outputs

--- pkg/utils/logger.go
+++ pkg/utils/logger.go
@@ -1,45 +1,73 @@
 package utils
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// ANSI color codes
+const (
+	ColorReset  = "\033[0m"
+	ColorRed    = "\033[31m"
+	ColorGreen  = "\033[32m"
+	ColorYellow = "\033[33m"
+	ColorBlue   = "\033[34m"
+	ColorWhite  = "\033[37m"
+	ColorGrey   = "\033[90m"
+)
+
+const (
+	VerbositySilent  = iota // 0: Only Fatal messages are logged.
+	VerbosityError          // 1: Error and Fatal messages are logged.
+	VerbosityWarning        // 2: Warning, Error, and Fatal messages are logged.
+	VerbosityInfo           // 3: Info, Warning, Error, and Fatal messages are logged.
+	VerbosityDebug          // 4: Debug, Info, Warning, Error, and Fatal messages are logged.
+)
 
 type DebugLogger struct {
 	verbose int
+	logger  *log.Logger
 }
 
-// verbose level 0: only errors
-// verbose level 1: errors and warnings
-// verbose level 2: errors, warnings and log messages
-// verbose level 3: errors, warnings, log messages and debug messages
-// verbose level 4: errors, warnings, log messages, debug messages and all messages
-
 func NewDebugLogger(verbose int) *DebugLogger {
-	return &DebugLogger{verbose: verbose}
+	customLogger := log.New(os.Stdout, "", 0)
+	return &DebugLogger{
+		verbose: verbose,
+		logger:  customLogger,
+	}
 }
 
 func (d *DebugLogger) Debug(format string, v ...interface{}) {
-	if d.verbose > 3 {
-		log.Printf("DEBUG: "+format, v...)
+	if d.verbose >= VerbosityDebug {
+		msg := fmt.Sprintf("[+] DEBUG: "+format, v...)
+		d.logger.Print(ColorGrey + msg + ColorReset)
 	}
 }
 
 func (d *DebugLogger) Info(format string, v ...interface{}) {
-	if d.verbose > 2 {
-		log.Printf("INFO : "+format, v...)
+	if d.verbose >= VerbosityInfo {
+		msg := fmt.Sprintf("[+] INFO: "+format, v...)
+		d.logger.Print(ColorGreen + msg + ColorReset)
 	}
 }
 
 func (d *DebugLogger) Warn(format string, v ...interface{}) {
-	if d.verbose > 1 {
-		log.Printf("WARN : "+format, v...)
+	if d.verbose >= VerbosityWarning {
+		msg := fmt.Sprintf("[+] WARN: "+format, v...)
+		d.logger.Print(ColorYellow + msg + ColorReset)
 	}
 }
 
 func (d *DebugLogger) Error(format string, v ...interface{}) {
-	if d.verbose > 0 {
-		log.Printf("ERROR: "+format, v...)
+	if d.verbose >= VerbosityError {
+		msg := fmt.Sprintf("[+] ERROR: "+format, v...)
+		d.logger.Print(ColorRed + msg + ColorReset)
 	}
 }
 
 func (d *DebugLogger) Fatal(format string, v ...interface{}) {
-	log.Fatalf("FATAL: "+format, v...)
+	msg := fmt.Sprintf("[+] FATAL: "+format, v...)
+	d.logger.Print(ColorRed + msg + ColorReset)
+	os.Exit(1)
 }
