4067bed0b772f955d04ddb1eec23d5fb3dbfc8cc
toby <toby.bro@pm.me>
2025-06-10 15:29:37 -0700

    chore: useless file

--- isolated/V3Descope/mod_hier_func_call.sv
+++ isolated/V3Descope/mod_hier_func_call.sv
@@ -2,13 +2,14 @@ module mod_hier_func_call (
     input logic i_value,
     output logic o_processed_value
 );
-    my_func_scope: begin
+    
     function automatic logic process_it (input logic val_in);
-      return ~val_in;
+        return ~val_in;
     endfunction : process_it
-      end : my_func_scope
-      always_comb begin
-    o_processed_value = my_func_scope.process_it(i_value);
-      end
+    
+    always_comb begin
+        o_processed_value = process_it(i_value);
+    end
+    
 endmodule
 
