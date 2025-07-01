462e9e499387a73ba465f836683982bfe49dbd49
toby <toby.bro@pm.me>
2025-07-01 15:12:36 -0700

    snippet: fix a wrong snippet

--- snippets/V3Cast.sv
+++ snippets/V3Cast.sv
@@ -39,19 +39,19 @@ module ConditionalAndCasts (
     assign out_explicit = 64'(out_conditional);
 endmodule
 module PackedStructOps (
-    input logic [15:0] packed_in,
     input logic [7:0] byte_val,
-    output logic [15:0] packed_out,
-    output logic [7:0] byte_out 
+    input logic [15:0] packed_in,
+    output logic [7:0] byte_out,
+    output logic [15:0] packed_out
 );
     typedef struct packed {
         logic [7:0] low;
         logic [7:0] high;
     } pair_t;
     pair_t data_pair;
-    assign data_pair = packed_in;
-    assign byte_out = data_pair.high;
+    assign data_pair.high = packed_in[15:8];
     assign data_pair.low = byte_val;
+    assign byte_out = data_pair.high;
     assign packed_out = data_pair;
     assign packed_out[7:0] = data_pair.low + byte_val;
 endmodule
